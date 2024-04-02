740f1590a9be916d86269159b6de0c8611949888
uzakotim <timauzakov@gmail.com>
2024-04-02 15:29:48 +0500

    colours: success

--- fedora/install_packages_for_skid_car.sh
+++ fedora/install_packages_for_skid_car.sh
@@ -3,11 +3,11 @@ NC='\033[0m' # No Color
 GRN='\033[0;32m'
 YLW='\033[1;33m'
 echo -e "${YLW}Installing packages for skid car${NC}"
-# sudo dnf install nodejs eigen3-devel zeromq-devel cppzmq-devel gcc-c++
+sudo dnf install nodejs eigen3-devel zeromq-devel cppzmq-devel gcc-c++
 echo -e "${GRN}Success${NC}"
 echo -e "${YLW}Installing pm2${NC}"
-# sudo npm -g install pm2
+sudo npm -g install pm2
 echo -e "${GRN}Success${NC}"
 echo -e "${YLW}Adding user to dialout group${NC}"
-# sudo usermod -a -G dialout $USER
+sudo usermod -a -G dialout $USER
 echo -e "${GRN}Installation complete${NC}"
